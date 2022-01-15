2e1e783e15ae64259a6fa6c010f4d9297cd1d959
mason ko <gym1029@gmail.com>
2022-01-15 12:45:57 +0900

    edit move subtitle test

--- main.go
+++ main.go
@@ -7,5 +7,6 @@ import (
 
 func main() {
 	fmt.Println("Hello")
-	recursive_move.MoveSubtitle("D:\\download\\_new\\[Moozzi2] Jujutsu Kaisen [ x265-10Bit Ver. ] - TV + SP")
+	recursive_move.MoveSubtitle("D:\\download\\_new\\[Moozzi2] Jujutsu Kaisen [ x265-10Bit Ver. ] - TV + SP",
+		[]string{"Jujutsu Kaisen", "mkv"}, []string{"呪術廻戦"})
 }
--- recursive_move/recursive.go
+++ recursive_move/recursive.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
+	"strings"
 )
 
 func MoveRecursive(rootDir, targetDir string, isRoot bool) {
@@ -33,13 +35,65 @@ func MoveRecursive(rootDir, targetDir string, isRoot bool) {
 	}
 }
 
-func MoveSubtitle(targetDir string) {
+func MoveSubtitle(targetDir string, movieKey, smiKey []string) {
 	dir, err := ioutil.ReadDir(targetDir)
 	if err != nil {
 		panic(err)
 	}
 
+	var movies []string
+	var smis []string
+
 	for _, v := range dir {
-		fmt.Println(v)
+		if v.IsDir() {
+			continue
+		}
+
+		name := v.Name()
+		ok := true
+		for _, k := range movieKey {
+			if !strings.Contains(name, k) {
+				ok = false
+			}
+		}
+		if ok {
+			movies = append(movies, name)
+		}
+
+		ok = true
+		for _, k := range smiKey {
+			if !strings.Contains(name, k) {
+				ok = false
+			}
+		}
+		if ok {
+			smis = append(smis, name)
+		}
 	}
+
+	sort.Strings(movies)
+	sort.Strings(smis)
+
+	if len(movies) != len(smis) {
+		return
+	}
+
+	for i, v := range movies {
+		smi := smis[i]
+		smiSp := strings.Split(smi, ".")
+		smiExt := smiSp[len(smiSp)-1]
+
+		movieSp := strings.Split(v, ".")
+		movieExt := movieSp[len(movieSp)-1]
+
+		fmt.Println(smiExt, movieExt)
+
+		newName := strings.Replace(v, "."+movieExt, "."+smiExt, 1)
+		fmt.Println(newName)
+
+		os.Rename(targetDir+"/"+smi, targetDir+"/"+newName)
+	}
+
+	fmt.Println(movies, len(movies))
+	fmt.Println(smis, len(smis))
 }
